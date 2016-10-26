5db593dcb37e335aed8c63d19ef24da5b5d37c2c
jsakamoto <junichi.jp@gmail.com>
2016-10-26 21:31:02 +0900

    xUnit.net edition - Separate version number information
    - from shared common file to individual "AssemblyInfo.cs".

--- ChainingAssertion.xUnit/Properties/AssemblyInfo.cs
+++ ChainingAssertion.xUnit/Properties/AssemblyInfo.cs
@@ -3,3 +3,17 @@ using System.Runtime.CompilerServices;
 using System.Runtime.InteropServices;
 
 [assembly: Guid("be9911db-89c2-4ec5-8f17-95ebb1df9f0e")]
+
+[assembly: AssemblyTitle("Chaining Assertion for xUnit - Binary Edition")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("neuecc")]
+[assembly: AssemblyProduct("ChainingAssertion")]
+[assembly: AssemblyCopyright("licensed under Microsoft Public License(Ms-PL)")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+
+[assembly: AssemblyVersion("1.7.3.0")]
+[assembly: AssemblyFileVersion("1.7.3.0")]
