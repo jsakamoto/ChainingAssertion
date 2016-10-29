26dd74ddbcf2dda8e73fdc50652f6e1e5259ba5c
jsakamoto <junichi.jp@gmail.com>
2016-10-29 21:57:05 +0900

    MSTest edition - Separate version number information
    - from shared common file to individual "AssemblyInfo.cs".

--- ChainingAssertion/Properties/AssemblyInfo.cs
+++ ChainingAssertion/Properties/AssemblyInfo.cs
@@ -3,3 +3,17 @@ using System.Runtime.CompilerServices;
 using System.Runtime.InteropServices;
 
 [assembly: Guid("86a0f6cc-4901-475a-bded-b061dd378bd6")]
+
+[assembly: AssemblyTitle("Chaining Assertion for MSTest - Binary Edition")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("neuecc")]
+[assembly: AssemblyProduct("ChainingAssertion")]
+[assembly: AssemblyCopyright("licensed under Microsoft Public License(Ms-PL)")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+
+[assembly: AssemblyVersion("1.8.0.0")]
+[assembly: AssemblyFileVersion("1.8.0.0")]
