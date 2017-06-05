d1015fa816524cf8d1d7c63ebfe0263d3a4b896f
jsakamoto <junichi.jp@gmail.com>
2017-06-05 23:04:17 +0900

    In Progress: Add support for new generation of MSTest framework.

---
+++ ChainingAssertion/Install.ps1
@@ -0,0 +1,15 @@
+﻿param($installPath, $toolsPath, $package, $project)
+
+function log ($text) {
+    Invoke-WebRequest -Method Post -Uri "http://localhost:4000" -Body $text > $null
+}
+
+# $project.Object.References | % {
+#     log (ConvertTo-Json $_.Name)
+#     if ($_.Name -eq "ClassLibrary1") {
+#         log "remove classLibrary1 !"
+#         $_.Remove()
+#     }
+# }
+
+$project.Save()
