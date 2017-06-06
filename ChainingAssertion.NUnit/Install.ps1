28f03fec04d1472994b7791eb6173836b6929b2b
jsakamoto <junichi.jp@gmail.com>
2017-06-06 22:52:20 +0900

    for NUnit - add support for both of NUnit v2 and v3.

---
+++ ChainingAssertion.NUnit/Install.ps1
@@ -0,0 +1,14 @@
+﻿param($installPath, $toolsPath, $package, $project)
+
+$nunitMajorVer = $project.Object.References | where { $_.Name -eq "nunit.framework" } | % { $_.MajorVersion }
+
+$project.Object.References | % {
+    if (($nunitMajorVer -ne 2) -and ($_.Name -eq "ChainingAssertion.NUnit")) {
+        $_.Remove()
+    }
+    if (($nunitMajorVer -ne 3) -and ($_.Name -eq "ChainingAssertion.NUnit3")) {
+        $_.Remove()
+    }
+}
+
+$project.Save()
