8dcf26c80c45749a420089d9b211babe3cf84474
jsakamoto <junichi.jp@gmail.com>
2017-06-06 21:33:13 +0900

    for MSTest - remove "printf" debug log from Install.ps1

--- ChainingAssertion/Install.ps1
+++ ChainingAssertion/Install.ps1
@@ -1,23 +1,14 @@
 ﻿param($installPath, $toolsPath, $package, $project)
 
-function log ($text) {
-    Invoke-WebRequest -Method Post -Uri "http://localhost:4000" -Body $text > $null
-}
-
 $refNames = $project.Object.References | % { $_.Name }
 $noVSQualityTools = -not ($refNames -contains "Microsoft.VisualStudio.QualityTools.UnitTestFramework")
 $noVSTestPlatform = -not ($refNames -contains "Microsoft.VisualStudio.TestPlatform.TestFramework")
-log "noVSQualityTools is $noVSQualityTools"
-log "noVSTestPlatform is $noVSTestPlatform"
 
 $project.Object.References | % {
-    log (ConvertTo-Json $_.Name)
     if ($noVSQualityTools -and $_.Name -eq "ChainingAssertion") {
-        log "-- remove ChainingAssertion.dll --"
         $_.Remove()
     }
     if ($noVSTestPlatform -and $_.Name -eq "ChainingAssertion.MSTest") {
-        log "-- remove ChainingAssertion.MSTest.dll --"
         $_.Remove()
     }
 }
--- ChainingAssertion/Properties/AssemblyInfo.cs
+++ ChainingAssertion/Properties/AssemblyInfo.cs
@@ -14,5 +14,5 @@ using System.Runtime.InteropServices;
 
 [assembly: ComVisible(false)]
 
-[assembly: AssemblyVersion("1.8.1.1")]
-[assembly: AssemblyFileVersion("1.8.1.1")]
+[assembly: AssemblyVersion("1.8.1.3")]
+[assembly: AssemblyFileVersion("1.8.1.3")]
